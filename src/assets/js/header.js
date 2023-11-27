9eee440e402207b803c7f4515688f4d2c5d6ef4b
Rominaruizdiaz <ruizdiazsanchezromina@gmail.com>
2023-11-28 00:15:33 +0100

    header finished

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,27 +1,32 @@
 import '../sass/header.scss';
-import FridaKhaloLogo from "../icons/logo-frida-khalo.svg";
+import FridaKhaloLogo from "../icons/frida-khalo-logo.webp";
+import SpainFlag from "../icons/spain-flag.svg";
+import downArrow from "../icons/arrow-down.svg";
 
 document.getElementById('header').innerHTML = `
   <header>
     <nav>
       <ul>
-        <a><li>INICIO</li></a>
-        <a><li>GALERÍA</li></a>
-        <a><li>VIDEOS</li></a>
-        <li><img src=${FridaKhaloLogo} alt="MDN"/></li>
-        <a><li>FRASES</li></a>
-        <a><li>CONTACTO</li></a>
-        <ul>
+        <a href="../../index.html"><li>INICIO</li></a>
+        <a href="../../pages/gallery/index.html"><li>GALERÍA</li></a>
+        <a href="../../pages/videos/index.html"><li>VIDEOS</li></a>
+        <li><img src=${FridaKhaloLogo} alt="MDN"></li>
+        <a href="../../pages/phrase/index.html"><li>FRASES</li></a>
+        <a href="../../pages/contact/index.html"><li>CONTACTO</li></a>
+        <ul id="language-dropdown">
           <div>
-            <span></span>
-            <button>IDIOMA</button>
+            <img src=${SpainFlag} alt="MDN">
+            <p>IDIOMA</p>
+            <img src=${downArrow} alt="MDN">
           </div>
-          <div>
-            <li>ESPAÑOL</li>
-            <li>INGLÉS</li>
-          </div>
+          <ul>
+            <div class="language">
+              <li id="spanish">ESPAÑOL</li>
+              <li id="english">INGLÉS</li>
+            </div>
+          </ul>
         </ul>
-      </ul>
+      </>
     </nav>
   </header>
 `;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './style.scss';
 import '../src/assets/js/header.js';
 import '../src/assets/js/footer.js';
+import '../src/assets/js/header.js';
