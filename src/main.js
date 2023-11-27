836fa523b92951a7c95a88f67fefd84538eb25ee
IsabelAM <isa_am_@hotmail.com>
2023-11-27 10:56:43 +0100

    insert fhotos

--- src/main.js
+++ src/main.js
@@ -1,29 +1,4 @@
 import './style.scss'
-/*import typescriptLogo from './assets/typescript.svg'
-import viteLogo from '../public/vite.svg'
-import { setupCounter } from './counter.ts'*/
-
-/*document.querySelector<HTMLDivElement>('#app')!.innerHTML = /* html  `
-<div>
-    <a href="https://vitejs.dev" target="_blank">
-    <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-    <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite</h1>
-    <div class="card">
-    <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-    Click on the Vite and TypeScript logos to learn more
-    </p>
-    <a href="/pages/gallery/">gallery</a> <span> / </span>
-    <a href="/pages/videos/">contact</a>
-</div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)*/
 
 let scale = 0;
 const cards = Array.from(document.getElementsByClassName("card"));
@@ -37,6 +12,7 @@ function slideAndScale() {
 		inner.style.transform = "translateX(" + scale * 8.5 + "em)";
 	});
 }
+
 
 (function init() {
 	slideAndScale();
