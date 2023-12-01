dbd80e1b9a1b0c0d3f532a3192c04266fe25b184
IsabelAM <isa_am_@hotmail.com>
2023-12-01 10:56:10 +0100

    insert page contact

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,1 +1,47 @@
-import '../../main';
+import "../../main.js";
+import "../../style.scss";
+import "../../assets/js/header.js";
+import "../../assets/js/footer.js";
+import "./sass/contact.scss";
+
+
+/*let scale = 0;
+const cards = Array.from(document.getElementsByClassName("card"));
+const inner = document.querySelector(".inner");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+function slideAndScale() {
+	cards.map((card, i) => {
+		card.setAttribute("data-scale", i + scale);
+		inner.style.transform = "translateX(" + scale * 8.5 + "em)";
+	});
+}
+
+
+(function init() {
+	slideAndScale();
+	cards.map((card, i) => {
+		card.textContent = " ";
+		card.addEventListener("click", () => {
+			const id = card.getAttribute("data-scale");
+			if (id !== 2) {
+				scale -= id - 2;
+				slideAndScale();
+			}
+		}, false);
+	});
+	
+	nextBtn.addEventListener("click", () => {
+		if (cards[cards.length -1].getAttribute("data-scale") != 2) {
+			scale--;
+		}
+		slideAndScale();
+	}, false);
+	prevBtn.addEventListener("click", () => {
+		if (cards[0].getAttribute("data-scale") < 2) {
+			scale++;
+		}
+		slideAndScale();
+	}, false);
+})();*/
